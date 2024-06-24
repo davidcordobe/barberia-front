173dda303f552d674400513175e41fd14e69c133
david arce <arce475@gmail.com>
2024-06-23 23:22:33 -0300

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import './app.css'; // Importa estilos generales de la aplicación
 const App = () => {
     return (
         <div className="app-container">
+            <img src="../src/assets/fondo/MASCHE BARBER.png" alt="Logo" />
             <h1>Reserva tu Turno!</h1>
             <div className="content">
                 <ReservaTurno />
-
             </div>
             <Footer />
         </div>
