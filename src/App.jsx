2174d99d039a6fe6762b174a5cbe18b13f0ee54f
david arce <arce475@gmail.com>
2024-06-27 10:11:54 -0300

    logo4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import ReservaTurno from './components/ReservarTurno';
 import Footer from './components/Footer';
-import './app.css'; // Importa estilos generales de la aplicación
-import logo from './logo/MASCHEBARBER.jpg'; // Importa la imagen
+import './app.css'; 
+import logo from './logo/MASCHEBARBER.png';
 
 const App = () => {
     return (
         <div className="app-container">
-            <img src={logo} alt="Masche Barber" /> {/* Usa la imagen importada */}
+            <img src={logo} alt="Masche Barber" /> 
             <h1>Reserva tu Turno!</h1>
             <div className="content">
                 <ReservaTurno />
